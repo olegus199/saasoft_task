328e6441e59f558e0560a15c4255bdd1ad716c38
olegus199 <okremnyov@yandex.ru>
2025-03-05 15:25:51 +0300

    init commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script
+    setup
+    lang="ts"
+>
+import Hint from "./components/Hint.vue";
+</script>
+
+<template>
+  <div class="heading">
+    <h1>Учетные записи</h1>
+    <button>
+      <i class="pi pi-plus" />
+    </button>
+  </div>
+  <Hint />
+</template>
+
+<style lang="scss">
+
+.heading {
+  display: flex;
+  align-items: center;
+}
+</style>
---
+++ src/components/Hint.vue
@@ -0,0 +1,26 @@
+<script
+    setup
+    lang="ts"
+>
+
+</script>
+
+<template>
+  <div class="hint-container">
+    <i class="pi pi-question-circle" />
+    <p class="hint-text">
+      Для указания нескольких меток для одной пары логин/пароль используйте
+      разделитель <b>;</b>
+    </p>
+  </div>
+</template>
+
+<style
+    scoped
+    lang="scss"
+>
+.hint-container {
+  display: flex;
+  align-items: center;
+}
+</style>
